9754051dc54ec43a8680679cf754ad5ca263aa67
lucasBaccaro1 <lucasbaccaro@hotmail.com>
2022-05-24 14:42:21 -0300

    10.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,24 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'project-cni',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  "plugins": {
+    "SplashScreen": {
+         "launchShowDuration": 1000,
+         "launchAutoHide": false,
+         "backgroundColor": "#ffffffff",
+         "androidSplashResourceName": "splash",
+         "androidScaleType": "CENTER_CROP",
+         "showSpinner": true,
+         "androidSpinnerStyle": "large",
+         "iosSpinnerStyle": "small",
+         "spinnerColor": "#999999",
+         "splashFullScreen": false,
+         "splashImmersive": false,
+         "layoutName": "launch_screen",
+         "useDialog": true
+    }
+  }
 };
 
 export default config;
